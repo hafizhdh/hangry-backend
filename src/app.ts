3808dcfadd59af92c1ac0e6979d1645405b331a7
Hafiz <hafizadhyaulhaq16@gmail.com>
2024-08-13 23:30:26 +0700

    Configure deployment

--- src/app.ts
+++ src/app.ts
@@ -41,8 +41,10 @@ const server = http.createServer((req, res) => {
   }
 })
 
-const PORT = process.env.PORT || 7878
+const PORT = parseInt(process.env.PORT || '7878')
+const env = process.env.NODE_ENV || "development"
+const host = env === "development" ? "localhost" : "0.0.0.0"
 
-server.listen(PORT, () => {
+server.listen(PORT, host, () => {
   console.log(`Listening on port ${PORT}`)
 })
