a42031b917acbf3144afe906e90e2a02517ce2bf
Hafiz <hafizadhyaulhaq16@gmail.com>
2024-08-13 20:33:16 +0700

    Add global handler

--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,9 @@ const server = http.createServer((req, res) => {
       getAllUser(req, res)
     } else if (req.method === 'POST') {
       createUser(req, res)
+    } else {
+      res.writeHead(404, {'Content-Type': 'application/json'})
+      res.end(JSON.stringify({"message": "Endpoint not found"}))
     }
   } 
   // /api/user/:id
@@ -22,11 +25,19 @@ const server = http.createServer((req, res) => {
       updateUser(req, res, id)
     } else if (req.method === 'DELETE') {
       deleteUser(req, res, id)
+    } else {
+      res.writeHead(404, {'Content-Type': 'application/json'})
+      res.end(JSON.stringify({"message": "Endpoint not found"}))
     }
-  } else {
+  }
+  // base url "/" 
+  else if (req.url === '/') {
     res.statusCode = 200
     res.setHeader('Content-Type', 'application/json')
     res.end(JSON.stringify({"message": "This is simple backend for managing user data"}))
+  } else {
+    res.writeHead(404, {'Content-Type': 'application/json'})
+    res.end(JSON.stringify({"message": "Endpoint not found"}))
   }
 })
 
